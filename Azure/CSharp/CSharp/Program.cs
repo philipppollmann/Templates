e65ee4cc4b16dce3c8feb0b7420b4340445ecfc1
Philipp <philipp@geuer-pollmann.de>
2023-07-29 15:32:01 +0200

    Change

--- Azure/CSharp/CSharp/Program.cs
+++ Azure/CSharp/CSharp/Program.cs
@@ -8,7 +8,7 @@ namespace CSharp
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
-            StorageAccountHandler.UploadFileToStorageAccount("", "DefaultEndpointsProtocol=https;AccountName=gitlabdemophil;AccountKey=+hFKkoRxSqvvsgghWbRNNMFA6vxLvMx7ax3FR7aTtSqKSj/Qh/3r3OGTNtWzqS1PBQiEwiHWTEkU+AStfCy17w==;EndpointSuffix=core.windows.net", "", "");
+            StorageAccountHandler.UploadFileToStorageAccount("", "", "", "");
         }
     }
     
